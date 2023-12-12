d2caa46cb6681c8a4bc5ccfcfdb1b20622551859
JohanaGikonyo <johanagikonyo552@gmail.com>
2023-12-12 21:56:30 +0300

    new

--- server/server.js
+++ server/server.js
@@ -4,51 +4,13 @@ const bodyParser = require('body-parser');
 const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
-const uploadDirectory = path.join(__dirname, 'uploads');
 
-const filename = '1. OS II Introduction.pdf';
-const safeFilename = path.join('uploads', filename);
-
+app.use(bodyParser.json());
 const bcrypt = require('bcrypt'); // Make sure you've added bcrypt to your project
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
-
-const filePath = path.join(uploadDirectory, '1. OS II Introduction.pdf');
-// Add a route to retrieve a list of available PDF files
-app.get('/get-pdfs', (req, res) => {
-  if (!fs.existsSync(uploadDirectory)) {
-    return res.status(500).send('Uploads directory does not exist.');
-  }
-
-  fs.readdir(uploadDirectory, (err, files) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send('Error reading PDF files.');
-    }
-    const pdfFiles = files.filter(file => file.endsWith('.pdf'));
-    res.json(pdfFiles);
-  });
-});
-
-app.use(bodyParser.urlencoded({ extended: true }));
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // Files will be stored in the 'uploads' directory
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname); // Use the original filename
-  },
-});
-
-const upload = multer({ storage });
-
-app.post('/upload', upload.single('pdfFile'), (req, res) => {
-  res.send('File uploaded successfully');
-});
-
-app.use('/pdfs', express.static('uploads'));
 
 // Connect to your MongoDB database
 mongoose.connect(MONGO_URI);
